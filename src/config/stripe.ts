6c2969a2dcc64890c134cc9b37e36cf91d23a74a
jeffvos <jeff.vos@aol.com>
2023-10-10 15:17:55 +0200

    init

---
+++ src/config/stripe.ts
@@ -0,0 +1,26 @@
+export const PLANS = [
+  {
+    name: "AnualPro",
+    slug: "pro",
+    quota: "unlimited",
+    price: {
+      amount: 2000,
+      priceIds: {
+        test: "price_1NzJP9Is9UaNMcKxcIKjbYX6",
+        production: "",
+      },
+    },
+  },
+  {
+    name: "Demo",
+    slug: "free",
+    quota: 177,
+    price: {
+      amount: 200,
+      priceIds: {
+        test: "price_1NzJTXIs9UaNMcKxSLqJeuRu",
+        production: "",
+      },
+    },
+  },
+];
